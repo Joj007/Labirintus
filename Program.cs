88582009aaafef7fa9a3ff6d0b8344d518df657c
mskvszkyt <71025319+mskvszkyt@users.noreply.github.com>
2023-01-12 10:02:19 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,52 +1,133 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
-namespace Labirintus
+namespace Labirintus_Projekt
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<string> CreateMap(string path)
         {
-            //Betöltés
-            Console.WriteLine("[1] 1. pálya");
-            Console.WriteLine("[2] 2. pálya");
-            Console.WriteLine("[3] 3. pálya");
-
-            int kulsoValtozo = 0;
-
-            while (true)
+            List<string> map = new List<string>();
+            map = File.ReadAllLines(path).ToList();
+            return map;
+        }
+        static bool IsMovementAvailable(char movementKey, int[] coordinate)
+        {
+            if (movementKey == 'w')
             {
-
-                Console.Write("Melyik pályát szeretné betölteni:");
-                int kivalasztott = Convert.ToInt32(Console.ReadLine());
-
-                if (kivalasztott == 1 || kivalasztott == 2 || kivalasztott == 3)
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Movement(int[] currentPosition)
+        {
+            char input = '´';
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                //Char keyChar = Convert.ToChar(key);
+                if (key.KeyChar == 'w' || key.KeyChar == 'a' || key.KeyChar == 's' || key.KeyChar == 'd')
                 {
-                    kulsoValtozo = kivalasztott;
+                    input = key.KeyChar;
                     break;
                 }
+            } while (true);
 
-                else
-                {
-                    Console.WriteLine("Hibás Bevitel!");
-                }
+            if (input == 'w')
+            {
+                Console.SetCursorPosition(currentPosition[0], currentPosition[1]);
+                Console.ResetColor();
+                Console.BackgroundColor = ConsoleColor.Red;
+
+                Console.SetCursorPosition(currentPosition[0] + 1, currentPosition[1]);
+                Console.ResetColor();
+
             }
 
-            Console.Clear();
-
-            var path = "Maps/map" + kulsoValtozo + ".txt";
-
-            string map = File.ReadAllText(path, Encoding.UTF8);
-
-            Console.WriteLine(map);
+            //if (IsMovementAvailable(input))
+            //{
+            //
+            //}
 
 
-            //Mentés
-            Console.Write("Kérem adja meg  pálya nevét:");
-            string mapName = Console.ReadLine();
-
-            string save = File.WriteAllLines();
         }
+
+        static void Timer(int time)
+        {
+            for (int a = time; a >= 0; a--)
+            {
+                Console.Write("\rHátralévő idő: {0:000}", a);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        static int[] StartingPosition(List<string> map)
+        {
+            DrawMap(map);
+
+            int[] startingPosition = new int[2];
+
+            for (int height = 0; height < map.Count; height++)
+            {
+                for (int width = 0; width < map[0].Length; width++)
+                {
+                    if (
+                        (height != 0 && height != map.Count && width == map[0].Length && map[height][width] == '═' || map[height][width] == '╚' || map[height][width] == '╔' || map[height][width] == '╩' || map[height][width] == '╦') || //jobb oldal
+
+                        (height != 0 && height != map.Count && width == 0 && map[height][width] == '═' || map[height][width] == '╗' || map[height][width] == '╝' || map[height][width] == '╩' || map[height][width] == '╦') || //bal oldal
+
+                        (height == map.Count && width == 0 && map[height][width] == '═' || map[height][width] == '╗' || map[height][width] == '╚' || map[height][width] == '╩' || map[height][width] == '║') || //jobb alsó sarok
+
+                        (height == map.Count && width == 0 && map[height][width] == '═' || map[height][width] == '╝' || map[height][width] == '╔' || map[height][width] == '╩' || map[height][width] == '║') || //bal alsó sarok
+
+                        (height == map.Count && width != 0 && width != map[0].Length && map[height][width] == '║' || map[height][width] == '╗' || map[height][width] == '╔' || map[height][width] == '╦') ||  //alsó rész
+
+                        (height == 0 && width == map[0].Length && map[height][width] == '═' || map[height][width] == '╝' || map[height][width] == '╔' || map[height][width] == '╦' || map[height][width] == '║') || //jobb felso sarok
+
+                        (height == 0 && width == 0 && map[height][width] == '═' || map[height][width] == '╗' || map[height][width] == '╚' || map[height][width] == '╦' || map[height][width] == '║') ||  //bal felso sarok
+
+                        (height == 0 && width != 0 && width != map[0].Length && map[height][width] == '║' || map[height][width] == '╝' || map[height][width] == '╚' || map[height][width] == '╩'))   //felső rész
+                    {
+                        Console.SetCursorPosition(width, height);
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Write(map[height][width]);
+                        Console.ResetColor();
+                        Console.SetCursorPosition(0, 20);
+                        startingPosition[0] = height;
+                        startingPosition[1] = width;
+
+                        return startingPosition;
+                    }
+                }
+            }
+            startingPosition[0] = -1;
+            startingPosition[1] = -1;
+
+            return startingPosition;
+        }
+        static void DrawMap(List<string> map)
+        {
+
+            for (int x = 0; x < map.Count; x++)
+            {
+                for (int y = 0; y < map[0].Length; y++)
+                {
+                    Console.Write(map[x][y]);
+                }
+                Console.Write("\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            StartingPosition(CreateMap("path"));
+            
+        }
+
     }
 }
